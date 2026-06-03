e84efe172e944bfd98e7e1bfca0a4d190c2f4dfe
mridulayadav07 <yadavmridula31@gmail.com>
2026-06-03 22:35:41 +0530

    Refactor code structure for improved readability and maintainability

--- C++DSA/Basic Hashing/CharHash.cpp
+++ C++DSA/Basic Hashing/CharHash.cpp
@@ -1,29 +1,24 @@
  #include<bits/stdc++.h>
  using namespace std;
  int main(){
-    int n;
-    cout << "Enter n: ";
-    cin >> n;
-    int arr[n] ;
-    for (int i= 0; i < n; i++){
-        cout << "Enter " << i << "th element: ";
-        cin >> arr[i];
-    }
+    string s;
+    cout << "Enter the string:";
+    cin >> s;
     //precompute
-    int hash[13] = {0};
-    for(int i = 0; i < n; i++){
-        hash[arr[i]] += 1;
+    int hash[256] = {0};
+    for(int i=0; i<s.size(); i++){
+        hash[s[i]]++;
     }
-
-    int q;
-    cout << "Enter the number of frequencies to be calculated:";
+    int q ;
+    cout << "Enter the number of characters whose frequency is to be calculated:";
     cin >> q;
     while(q--){
-        int number;
-        cout << "Enter the number of which frequencies are to be calculated: ";
-        cin >> number;
+        char c;
+        cout << "Enter the character whose frequency is to be calculated:";
+        cin >> c;
         //fetch
-        cout << hash[number] << endl;
+        cout << hash[c] <<endl;
     }
+
     return 0;
  } 
---
+++ C++DSA/Basic Hashing/HashMap.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+ using namespace std;
+
+ int main(){
+    int n;
+    cout << "Enter the array size:";
+    cin >> n;
+    int arr[n];
+    for(int i = 0; i < n; i++){
+        cout << "Enter " << i << "th element of array:";
+        cin >> arr[i];
+    }
+    //precompute
+    map<int,int> mpp;
+    for(int i = 0; i < n; i++){
+        mpp[arr[i]]++;
+    }
+    int q;
+    cout << "Enter the number of queries:";
+    cin >> q;
+    while(q--){
+        int number;
+        cout << "Enter the query:";
+        cin >> number;
+        //fetch
+        cout << mpp[number]<< endl;
+    }
+    return 0;
+ } 
