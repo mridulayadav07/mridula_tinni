87fa9baa2a552072d0c3d12152b0d460082b7794
mridulayadav07 <yadavmridula31@gmail.com>
2026-06-06 00:03:59 +0530

    Add implementation for removing duplicates from a sorted array
    - Created a new file 4.RemoveDuplicate.cpp
    - Implemented the removeDuplicates function to modify the input vector in place
    - Added a main function to handle user input and display results

---
+++ C++DSA/Array/4.RemoveDuplicate.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int removeDuplicates(vector<int>& nums) {
+    if(nums.size() == 0) return 0;
+
+    int i = 0;
+
+    for(int j = 1; j < nums.size(); j++){
+        if(nums[j] != nums[i]){
+            nums[i + 1] = nums[j];
+            i++;
+        }
+    }
+
+    return i + 1;
+}
+
+int main() {
+    int n;
+    cout << "Enter size of array: ";
+    cin >> n;
+
+    vector<int> nums(n);
+
+    cout << "Enter elements (sorted): ";
+    for(int i = 0; i < n; i++){
+        cin >> nums[i];
+    }
+
+    int newSize = removeDuplicates(nums);
+
+    cout << "Array after removing duplicates: ";
+    for(int i = 0; i < newSize; i++){
+        cout << nums[i] << " ";
+    }
+
+    cout << "\nNew size: " << newSize;
+
+    return 0;
+}
