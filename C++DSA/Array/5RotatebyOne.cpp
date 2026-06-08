2157e50172e062db1d08b3df9a6bdd10dc1023b0
mridulayadav07 <yadavmridula31@gmail.com>
2026-06-08 19:55:23 +0530

    Add implementations for removing duplicates and rotating an array

---
+++ C++DSA/Array/5RotatebyOne.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+ using namespace std;
+void rotateArray(vector<int>& arr , int n){
+    int temp = arr[0];
+    for(int i = 1; i < n; i++){
+        arr[i - 1] = arr[i];
+    }
+    arr[n - 1] = temp;
+}
+ int main(){
+    int n;
+    cout << "Enter the size of array:";
+    cin >> n;
+    vector<int> arr(n);
+    for(int i = 0; i < n; i++){
+        cout << "Enter " << i << "th element of array:";
+        cin >> arr[i];
+    } 
+    rotateArray(arr,n);
+    cout << "Rotated Array is:";
+    for(int i=0;i<n;i++){
+        cout << arr[i] << " | ";
+    }
+    return 0;
+ } 
