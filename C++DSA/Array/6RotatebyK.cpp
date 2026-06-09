37c3c19caf2290f48a0dc80e45930aac4765a729
mridulayadav07 <yadavmridula31@gmail.com>
2026-06-09 23:01:19 +0530

    Refactor code structure for improved readability and maintainability

---
+++ C++DSA/Array/6RotatebyK.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+ using namespace std;
+ void reverse(int arr[],int start,int end){
+    while(start<=end){
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+        start++;
+        end--;
+    }
+ }
+void rotateArray(int arr[] , int n,int d){
+    reverse(arr,0,n-d-1);
+    reverse(arr,n-d,n-1);
+    reverse(arr,0,n-1);
+
+}
+ int main(){
+    int n;
+    cout << "Enter the size of array:";
+    cin >> n;
+    int arr[n];
+    for(int i = 0; i < n; i++){
+        cout << "Enter " << i << "th element of array:";
+        cin >> arr[i];
+    } 
+    int k;
+    cout << "Enter the value of k:";
+    cin >> k;
+    rotateArray(arr,n,k);
+    cout << "Rotated Array is:";
+    for(int i=0;i<n;i++){
+        cout << arr[i] << " | ";
+    }
+    return 0;
+ } 
